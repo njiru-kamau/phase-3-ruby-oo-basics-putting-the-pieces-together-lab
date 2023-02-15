2a02a426aef2e22f347b76424ad8fdfc350192ef
njiru-kamau <maureenkamau947@gmail.com>
2023-02-15 08:50:43 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
-
+    def initialize(title = "The Alchemist")
+        @title = title
+    end
+    def turn_page 
+        puts "This book is good!"
+    end
+end    
